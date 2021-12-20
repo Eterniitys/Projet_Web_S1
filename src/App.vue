809fa8d3705b7db5b2ea73f9e1dae0cf38a24239
Hippolyte Guillemeteau <HippolyteG16@gmail.com>
2021-12-20 03:25:38 +0100

    cursor click to jump

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <a-scene embedded arjs>
     <a-marker preset='hiro'>
       <!-- use your gltf model -->
+      <a-entity id="mouseCursor" cursor="rayOrigin: mouse"></a-entity>
       <a-plane position="1 0 0" rotation="-90 0 0" width="1" height="10" color="#7BC8A4" static-body></a-plane>
       <a-plane position="0 0 0" rotation="-90 0 0" width="1" height="10" color="#37afb8" static-body></a-plane>
       <a-plane position="-1 0 0" rotation="-90 0 0" width="1" height="10" color="#7BC8A4" static-body></a-plane>
@@ -9,9 +10,11 @@
       <a-cylinder position="0.5 0.2 -15" color="crimson" height="2" radius="0.2" rotation="90 0 90" animation="property: position; dur: 12500; to: 0.5 0.2 10; loop: true"></a-cylinder>
       <a-cylinder position="-0.5 0.2 -20" color="crimson" height="2" radius="0.2" rotation="90 0 90" animation="property: position; dur: 15000; to: -0.5 0.2 10; loop: true"></a-cylinder>
       <a-cylinder position="0 0.2 -25" color="crimson" height="3" radius="0.2" rotation="90 0 90" animation="property: position; dur: 17500; to: 0 0.2 10; loop: true"></a-cylinder>
-      <a-cylinder position="0 0.5 0" rotation="0 0 0" height="1" radius="0.2" static-body></a-cylinder>
+      <a-cylinder position="0 0.5 0" rotation="0 0 0" height="1" radius="0.2" static-body
+        animation__position="property: position; from: 0 1.5 0; to: 0 0.5 0; dur: 500; startEvents: click;">
+      </a-cylinder>
     </a-marker>
-    <a-entity camera ></a-entity>
+    <a-entity camera ><a-cursor></a-cursor></a-entity>
   </a-scene>
 </template>
 
